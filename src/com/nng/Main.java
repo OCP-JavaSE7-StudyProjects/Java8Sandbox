2badeddc8d3bedc8bac8780f51ca0cdda725cdf3
Zsuzsanna Magyar <zsmagyar@users.noreply.github.com>
2016-04-30 21:06:29 +0200

    6: stream and lambda version of "find the double of the first even nuber  that is greater than 3" task

--- src/com/nng/Main.java
+++ src/com/nng/Main.java
@@ -7,14 +7,13 @@ public class Main {
 
     public static void main(String[] args) {
         List<Integer> values = Arrays.asList(1, 2, 3, 5, 4, 6, 7, 8, 9, 10);
-        int result = 0;
-        for (int e : values) {
-            if (e > 3 && e % 2 == 0) {
-                result = e * 2;
-                break;
-            }
-        }
-            System.out.println(result);
-        }
-
+        System.out.println(
+                values.stream()
+                        .filter(e -> e > 3)
+                        .filter(e -> e % 2 == 0)
+                        .map(e -> e * 2)
+                        .findFirst()
+                        .get());//without this we get an Optional(8)
     }
+
+}
