812c5f603f14b06e8bf54c14f2f516c975691c92
Zsuzsanna Magyar <zsmagyar@users.noreply.github.com>
2016-04-30 20:49:21 +0200

    5: basecode of "find the double of the first even nuber greater than 3 " task

--- src/com/nng/Main.java
+++ src/com/nng/Main.java
@@ -1,23 +1,20 @@
 package com.nng;
 
-import java.util.function.IntPredicate;
-import java.util.function.Predicate;
-import java.util.stream.IntStream;
+import java.util.Arrays;
+import java.util.List;
 
 public class Main {
 
     public static void main(String[] args) {
-        System.out.println(isPrime(1));
-        System.out.println(isPrime(2));
-        System.out.println(isPrime(3));
-        System.out.println(isPrime(4));
-    }
+        List<Integer> values = Arrays.asList(1, 2, 3, 5, 4, 6, 7, 8, 9, 10);
+        int result = 0;
+        for (int e : values) {
+            if (e > 3 && e % 2 == 0) {
+                result = e * 2;
+                break;
+            }
+        }
+            System.out.println(result);
+        }
 
-    private static boolean isPrime(final int number) {
-        IntPredicate isDivisible = divisor -> number % divisor == 0;
-
-        return number > 1 &&
-                IntStream.range(2, number)
-                        .noneMatch(isDivisible);
     }
-}
