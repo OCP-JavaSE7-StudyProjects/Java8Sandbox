c9690b0f23ad62542391d5d22ff01d8e4f724023
Zsuzsanna Magyar <zsmagyar@users.noreply.github.com>
2016-04-30 23:41:52 +0200

    8:"sum values"  task base (lambdas <-> anonymous class implementation of a one abstract method container interface/abstract class)

--- src/com/nng/Main.java
+++ src/com/nng/Main.java
@@ -6,43 +6,31 @@ import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Stream;
 
+interface Selector {
+    public boolean pick(int value);
+}
+//this is soo ugly and indokolatlanul long
+//class EvenSelector implements Selector {
+//    @Override
+//    public boolean pick(final int value) {
+//        return value % 2 == 0;
+//    }
+//}
+
 public class Main {
-    public static boolean isEven(int number) {
-        System.out.println("isEven " + number);
-        return number % 2 == 0;
+    public static int sumValues(List<Integer> numbers, Selector selector) {
+        int result = 0;
+        for (int e : numbers) {
+            if (selector.pick(e)) result += e;
+        }
+        return result;
     }
 
-    public static int doubleIt(int number) {
-        System.out.println("doubleIt " + number);
-        return number * 2;
-    }
 
     public static void main(String[] args) {
         List<Integer> values = Arrays.asList(1, 2, 3, 5, 4, 6, 7, 8, 9, 10);
 
-        Function<Integer, Predicate<Integer>> isGreaterThan =
-                pivot ->
-                        number ->
-                //style with just a single operation
-                              //  number > pivot;
-                // style with not just one operation:
-                                {
-                                    System.out.println("isGreaterThan" + pivot + " " + number);
-                                    return number > pivot;
-                                };
-
-        final Stream<Integer> streamPipe = values.stream()
-                .filter(isGreaterThan.apply(3))
-                .filter(Main::isEven)//if clear what is the parameter, we can use like this
-                .map(Main::doubleIt);
-
-        System.out.println(//without calling this, nothing will happen (!!lazy evaluation)
-                streamPipe
-                        .findFirst()
-                        .get()//without this we get an Optional(8)
-
-        );
-
+        System.out.println(sumValues(values, e -> e % 2 == 0));
     }
 
 }
