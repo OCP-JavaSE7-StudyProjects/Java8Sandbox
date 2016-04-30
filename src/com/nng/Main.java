1e683b21eb38dc432fdcc9b8e90b8352d6ea7533
Zsuzsanna Magyar <zsmagyar@users.noreply.github.com>
2016-04-30 19:46:37 +0200

    Base  (is prime with for)

---
+++ src/com/nng/Main.java
@@ -0,0 +1,18 @@
+package com.nng;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(isPrime(1));
+        System.out.println(isPrime(2));
+        System.out.println(isPrime(3));
+        System.out.println(isPrime(4));
+    }
+
+    private static boolean isPrime(final int number) {
+        for (int i = 2; i < number; i++) {
+            if (number % i == 0) return false;
+        }
+        return number > 1;
+    }
+}
