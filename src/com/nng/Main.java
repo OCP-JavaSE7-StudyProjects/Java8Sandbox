de0c8d0158bbb8309054c543d11e0f7333a3bc3a
Zsuzsanna Magyar <zsmagyar@users.noreply.github.com>
2016-04-30 23:58:59 +0200

    10: paralell stream

--- src/com/nng/Main.java
+++ src/com/nng/Main.java
@@ -5,17 +5,26 @@ import java.util.List;
 import java.util.function.Predicate;
 
 public class Main {
-    public static int sumValues(List<Integer> numbers, Predicate<Integer> selector) {
-       return numbers.stream()
-               .filter(selector)
-               .reduce(0,Math::addExact);
+    public static int doubleIt(int number) {
+        //simulatnig complex calculation in time
+        try {Thread.sleep(1000);} catch (Exception ex) {}
+        return number * 2;
     }
 
 
     public static void main(String[] args) {
-        List<Integer> values = Arrays.asList(1, 2, 3, 5, 4, 6, 7, 8, 9, 10);
+        List<Integer> values = Arrays.asList(1, 2, 3, 5, 4, 6);
 
-        System.out.println(sumValues(values, e -> e % 2 == 0));
+        System.out.println(
+                values.stream()
+                        .mapToInt(Main::doubleIt)
+                        .sum());
+
+        //referential transparency needed ("clean function")
+        System.out.println(
+                values.parallelStream()
+                        .mapToInt(Main::doubleIt)
+                        .sum());
     }
 
 }
